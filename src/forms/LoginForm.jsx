cc637c04967af2fa4d16a9969cf52d4a60029ff1
Alina Rusu <alex.alina.rusu@gmail.com>
2023-03-01 18:52:51 -0500

    Draft Login screen and update screens spacing

--- src/forms/LoginForm.jsx
+++ src/forms/LoginForm.jsx
@@ -1,27 +1,24 @@
 import { Form, Formik } from 'formik';
 import React from 'react';
-import { useDispatch } from 'react-redux';
-import { createUser } from '../actions-reducers/users';
+// import { useDispatch } from 'react-redux';
+// import { createUser } from '../actions-reducers/users';
 import Button from '../core-components/Button';
-import { signupScreen } from '../text/text';
-import { validateSignupForm } from '../utils/validation';
+import { loginScreen } from '../text/text';
+// import { validateSignupForm } from '../utils/validation';
 import { TextField } from './Fields';
 
-const SignUpForm = () => {
-  const dispatch = useDispatch();
+const LoginForm = () => {
+  // const dispatch = useDispatch();
 
   return (
     <Formik
       initialValues={{
         email: '',
-        firstName: '',
-        lastName: '',
         password: '',
-        confirmPassword: '',
       }}
-      validate={(values) => validateSignupForm(values)}
+      // validate={(values) => validateSignupForm(values)}
       onSubmit={(values, actions) => {
-        dispatch(createUser(values));
+        // dispatch(createUser(values));
         actions.setSubmitting(false);
       }}
     >
@@ -35,35 +32,15 @@ const SignUpForm = () => {
             }}
           >
             <TextField
-              name="firstName"
-              type="text"
-              label="First Name"
-              placeholder="First Name"
-            />
-            <TextField
-              name="lastName"
-              type="text"
-              label="Last Name"
-              placeholder="Last Name"
-            />
-            <TextField
               name="email"
               type="email"
               label="Email"
               placeholder="janedoe@example.com"
             />
-
             <TextField name="password" type="password" label="Password" />
-            <TextField
-              name="confirmPassword"
-              type="password"
-              label="Confirm Password"
-            />
-
             <Button
               variant="primaryLg"
-              mt={[3, 3, 3, 5, 5]}
-              mb={6}
+              my={[4, 4, 3, 5, 5]}
               mx="auto"
               disabled={isSubmitting}
               type="submit"
@@ -72,7 +49,7 @@ const SignUpForm = () => {
                 submitForm();
               }}
             >
-              {signupScreen.signupBtn}
+              {loginScreen.loginBtn}
             </Button>
           </Form>
         );
@@ -81,4 +58,4 @@ const SignUpForm = () => {
   );
 };
 
-export default SignUpForm;
+export default LoginForm;
