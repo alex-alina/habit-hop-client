f85f2247795e5569fb1acd4622d43e4cdaa67d1e
Alina Rusu <alex.alina.rusu@gmail.com>
2023-04-05 18:17:21 -0400

    Add tests for validate name, password and confirmPasword utils

--- src/utils/__tests__/validation.js
+++ src/utils/__tests__/validation.js
@@ -1,8 +1,8 @@
 import {
   validateEmail,
-  // validateName,
-  // validatePassword,
-  // validateConfirmPassword,
+  validateName,
+  validatePassword,
+  validateConfirmPassword,
   // validateGoalPriority,
   // validateTextBlocks,
   // validateEndDateInput,
@@ -30,7 +30,7 @@ const invalidEmails = [
   'berry@gmail..com',
 ];
 
-describe('Validate Email Error message', () => {
+describe('Validate Email', () => {
   validEmails.map((email, index) => {
     it(`should return undefined for valid email with index ${index}`, () => {
       expect(validateEmail(email)).toBe(undefined);
@@ -44,5 +44,53 @@ describe('Validate Email Error message', () => {
 
   it('should return "Required" error message when no value is passed', () => {
     expect(validateEmail()).toBe('Required');
+  });
+});
+
+describe('Validate Name', () => {
+  it(`should return undefined for valid name`, () => {
+    expect(validateName('Mike Mo')).toBe(undefined);
+  });
+
+  it(`should return error message for values with less then 2 chars`, () => {
+    expect(validateName('J')).toBe('Name must have at least 2 characters');
+  });
+
+  it('should return "Required" error message when no value is passed', () => {
+    expect(validateName()).toBe('Required');
+  });
+});
+
+describe('Validate Password', () => {
+  it(`should return undefined for valid pasword`, () => {
+    expect(validatePassword('12crownNine')).toBe(undefined);
+  });
+
+  it(`should return error message for values with less then 8 chars`, () => {
+    expect(validatePassword('bee4s')).toBe(
+      'Pasword must have at least 8 characters'
+    );
+  });
+
+  it('should return "Required" error message when no value is passed', () => {
+    expect(validatePassword()).toBe('Required');
+  });
+});
+
+describe('Validate Confirm Password', () => {
+  const password = '12crownNine';
+  const confirmPassword = '12crownNine';
+  it(`should return undefined for valid pasword`, () => {
+    expect(validateConfirmPassword(password, confirmPassword)).toBe(undefined);
+  });
+
+  it(`should return error message for values with less then 8 chars`, () => {
+    expect(validateConfirmPassword(password, '12crowNine')).toBe(
+      "Passwords don't match"
+    );
+  });
+
+  it('should return "Required" error message when no value is passed', () => {
+    expect(validateConfirmPassword()).toBe('Required');
   });
 });
