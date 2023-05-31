f7c94d1ca58cd478be7cc88153d228060a548f09
Alina Rusu <alex.alina.rusu@gmail.com>
2023-05-31 09:56:13 -0400

    update exports for mock constants variables

--- src/mocks/constants.js
+++ src/mocks/constants.js
@@ -1,7 +1,7 @@
-export const storageJwtKey = 'jwt';
-export const mockJwt =
+const storageJwtKey = 'jwt';
+const mockJwt =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImRjM2MxMWJmLTU4NGMtNDA5MC1hYThiLTM5ZTQzY2ZhZmNhYiIsImlhdCI6MTY4MTQyMDcwMCwiZXhwIjoxNjgxNDI3OTAwfQ.sYclsZo_DO9zZ2375Kg-AiGZpiVvox1NojPK3eGj9k4';
-export const mockUserData = {
+const mockUserData = {
   createdAt: '2023-04-25T19:08:15.956Z',
   id: 'dc3c11bf-584c-4090-aa8b-39e43cfafcab',
   email: 'test@example.com',
@@ -10,7 +10,7 @@ export const mockUserData = {
   lastLogin: '2023-04-25T19:08:15.956Z',
 };
 
-export const mockGoalFormData = {
+const mockGoalFormData = {
   goalDefinition:
     'Improve sleep quality by exercising and drinking less coffee',
   priority: 'main',
@@ -18,7 +18,7 @@ export const mockGoalFormData = {
   endDate: '2023-06-16',
 };
 
-export const mockGoalData1 = {
+const mockGoalData1 = {
   endDate: '2023-05-06',
   goalDefinition:
     'Improve sleep quality by exercising and drinking less coffee',
@@ -27,7 +27,7 @@ export const mockGoalData1 = {
   startDate: '2023-04-08',
 };
 
-export const mockGoalData2 = {
+const mockGoalData2 = {
   endDate: '2023-05-06',
   goalDefinition:
     'I want to learn how to play guitar, and be able to play 3 songs by the end of the year',
@@ -36,7 +36,7 @@ export const mockGoalData2 = {
   startDate: '2023-04-08',
 };
 
-export const mockGoalData3 = {
+const mockGoalData3 = {
   endDate: '2023-05-06',
   goalDefinition:
     'I want to learn how to bake muffins, cupcakes and birthday cakes by baking every weekend for six months',
@@ -45,7 +45,7 @@ export const mockGoalData3 = {
   startDate: '2023-04-08',
 };
 
-export const editGoalData = {
+const editGoalData = {
   endDate: '2023-05-06',
   goalDefinition:
     'Train for a tripple marathon by running every day for 15km and going to the gym twice per week',
@@ -54,13 +54,13 @@ export const editGoalData = {
   startDate: '2023-04-08',
 };
 
-export const mockHabitFormData = {
+const mockHabitFormData = {
   habitDescription: 'Exercise daily',
   habitType: 'develop',
   progressMetric: 'minutes',
 };
 
-export const mockHabitData = {
+const mockHabitData = {
   id: '66c8b782-6238-42a2-ab77-2c03d19b1d0f',
   habitDescription: 'Exercise daily',
   habitType: 'develop',
@@ -74,7 +74,7 @@ export const mockHabitData = {
   },
 };
 
-export const mockHabitsData = [
+const mockHabitsData = [
   {
     id: 'a4e95781-9f45-4c38-8d67-21355fd5e228',
     habitDescription: 'Run 2 km every evening',
@@ -105,7 +105,7 @@ export const mockHabitsData = [
   },
 ];
 
-export const fourHabitsMockData = [
+const fourHabitsMockData = [
   {
     id: 'a4e95781-9f45-4c38-8d67-21355fd5e228',
     habitDescription: 'Run 2 km every evening',
@@ -161,3 +161,18 @@ export const fourHabitsMockData = [
     },
   },
 ];
+
+export {
+  editGoalData,
+  fourHabitsMockData,
+  storageJwtKey,
+  mockJwt,
+  mockGoalData1,
+  mockGoalData2,
+  mockGoalData3,
+  mockGoalFormData,
+  mockHabitData,
+  mockHabitFormData,
+  mockHabitsData,
+  mockUserData,
+};
